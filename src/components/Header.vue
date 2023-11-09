fbb33aea88ec853bae1129f7557d8f6fac70a95f
Vourlidas <nvourlidas@gmail.com>
2023-11-09 17:30:59 +0200

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<!-- Header.vue -->
+<template>
+    <header>
+      <div class="container">
+        <div class="logo">
+          <router-link to="/"><img src="src/assets/cropped-logo_transparent-1.png"></router-link>
+        </div>
+        <nav>
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <!-- Add more navigation links as needed -->
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    // You can add component-specific logic here if needed
+  };
+  </script>
+  
+  <style>
+  /* Add styling for your header */
+  header {
+    background-color: rgba(0, 0, 0, 0.5); /* Transparent black background */
+    color: #fff;
+    padding: 10px 0; /* Adjust the padding as needed */
+  }
+  
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px; /* Set your desired max-width */
+    margin: 0 auto; /* Center the content */
+  }
+  
+  .logo {
+    font-size: 1.5rem; /* Adjust the font size */
+  }
+  
+  nav ul {
+    list-style: none;
+    display: flex;
+  }
+  
+  nav li {
+    margin-right: 20px;
+  }
+  
+  nav a {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+  }
+  
+  /* Media query for responsiveness */
+  @media (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  
+    nav {
+      margin-top: 10px;
+    }
+  }
+  </style>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="parallax" id="home">
+    <div class="content">
+      <h1>ComHouse</h1>
+      <p>We build awesome websites and apps</p>
+    </div>
+  </section>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+      scrollPosition: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+};
+</script>
+  
+  
+<style>
+.parallax {
+  /* background-image: url('your-background-image.jpg'); */
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.content {
+  text-align: center;
+  color: rgb(2, 0, 0);
+}
+
+.content h1 {
+  font-size: 2.5rem;
+}
+
+.content p {
+  font-size: 1.5rem;
+}
+</style>
