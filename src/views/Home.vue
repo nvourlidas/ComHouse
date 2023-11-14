a97ab36de56c96ce72b7e869a46ffef90cd78020
Nvourlidas <nvourlidas@gmail.com>
2023-11-14 21:56:30 +0200

    14/11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,52 @@
 <template>
-  <Header class="stick"/>
-  <Main class="mn"/>
-  <section class="parallax" id="home">
-    <div class="content">
-      <h1>ComHouse</h1>
-      <p>We build awesome websites and apps</p>
-    </div>
-  </section>
+  <div v-if="isLoading" class="loading-spinner fadeOut">
+    <Loader />
+  </div>
+  <div v-else class="home fadeIn">
+    <Header class="stick" :style="{ backgroundColor: headerBackgroundColor }" />
+    <Main class="mn" />
+
+    <section class="parallax" id="home">
+      <div class="content">
+        <h1>ComHouse</h1>
+        <p>We build awesome websites and apps</p>
+      </div>
+    </section>
+  </div>
 </template>
   
 <script>
 import Header from '@/components/Header.vue';
 import Main from '@/components/Main.vue';
+import Loader from '@/components/Loader.vue';
 export default {
   components: {
     Header,
-    Main
+    Main,
+    Loader
   },
   data() {
     return {
-      scrollPosition: 0,
+      headerBackgroundColor: "transparent",
+      isLoading: true,
     };
   },
   mounted() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll);
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 4500);
   },
-  beforeUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
     handleScroll() {
-      this.scrollPosition = window.scrollY;
+      const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+      this.headerBackgroundColor = `rgba(11, 64, 156, ${scrollPercentage / 100})`;
     },
   },
+
 };
 </script>
   
@@ -61,10 +75,62 @@ export default {
   font-size: 1.5rem;
 }
 
-.stick{
+.stick {
   position: fixed;
+  top: 0;
+  transition: background-color 0.3s ease;
+  width: 100%;
+}
+
+.loading-spinner {
+  /* Style your loading spinner here */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 1.5rem;
+  -webkit-animation-duration: 10s;
+  animation-duration: 20s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
 }
 
 
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+.fadeOut {
+  -webkit-animation-name: fadeOut;
+  animation-name: fadeOut;
+}
+
+.home {
+  -webkit-animation-duration: 3s;
+  animation-duration: 3s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.fadeIn{
+  -webkit-animation-name: fadeIn;
+  animation-name: fadeIn;
+}
 
 </style>
